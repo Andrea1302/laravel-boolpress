96615f22548c1dff0757dcfe701d973d810af520
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2022-02-08 10:28:14 +0100

    fixed report error

--- database/seeds/categorieSeeder.php
+++ database/seeds/categorieSeeder.php
@@ -12,6 +12,6 @@ class categorieSeeder extends Seeder
      */
     public function run()
     {
-        factory(Category::class, 50) -> create();        
+        factory(Category::class, 10) -> create();        
     }
 }
